010699d98ec65c285506448028826c4e61394110
yemmyharry <yemmyharry@gmail.com>
2020-10-17 21:13:57 +0100

    added controller for users

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,11 @@ const router = express.Router()
 const { user_signup, user_login, delete_user } = require('../controllers/user')
 const checkAuth = require('../middleware/check-auth')
 
-
 router.post('/signup', user_signup)
 
 router.post('/login', user_login)
 
-
 router.delete('/:userId', checkAuth, delete_user)
-
 
 
 module.exports = router;
